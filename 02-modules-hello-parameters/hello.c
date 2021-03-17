be075f2b7a330fcb1c49172e86a6b8d2cd86f570
Gabriele Proietti Mattia <pm.gabriele@outlook.com>
2021-03-17 15:27:45 +0100

    Completed example 2 for modules

--- 02-modules-hello-parameters/hello.c
+++ 02-modules-hello-parameters/hello.c
@@ -2,40 +2,30 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#define MODULE_LOG "hello: "
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Gabriele Proietti Mattia <pm.gabriele@outlook.com>");
 MODULE_DESCRIPTION("My first hello world module");
 MODULE_VERSION("1.0.0");
 
-short myshort;
-module_param(myshort, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-MODULE_PARM_DESC(myshort, "A short integer");
-
-short myint;
+int myint = -1;
 module_param(myint, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 MODULE_PARM_DESC(myint, "An integer");
-
-short mylong;
-module_param(mylong, long, S_IRUSR);
-MODULE_PARM_DESC(mylong, "A long integer");
-
-short mystring;
-module_param(mystring, charp, 0000);
-MODULE_PARM_DESC(mystring, "A character string");
-
-short myintarray;
-module_param_array(myintArray, int, &arr_argc, 0000);
-MODULE_PARM_DESC(myintArray, "An array of integers");
 
 static int __init dummy_init(void)
 {
     printk(KERN_INFO "Hello, this is my first kernel module \n");
+
+    printk(KERN_INFO MODULE_LOG "this is a correct print\n");
+    printk(KERN_WARNING MODULE_LOG "Variable myint = %d\n", myint);
+
     return 0;
 }
 
 static void __exit dummy_exit(void)
 {
-    printk(KERN_INFO "Exit Hello world module\n");
+    printk(KERN_INFO MODULE_LOG "module exited\n");
 }
 
 module_init(dummy_init);
